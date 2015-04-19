ac4e36aab7c692ae49e1f761cd8ea3361be2abcc
Nathaniel Birrer <natbirrer@gmail.com>
2015-04-18 21:03:05 -0400

    First commit. Add README and main.cpp.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    while(true){
+        // main game loop
+        // Get input character for the turn
+        // switch? on input
+    }
+}
